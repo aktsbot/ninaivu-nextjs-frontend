5230d0656db1bdeee7ad693c90f5e3da4df79f58
aktsbot <box.ashishk@gmail.com>
2023-03-13 01:29:38 +0530

    add auth check for all routes

---
+++ middleware.ts
@@ -0,0 +1,5 @@
+// https://next-auth.js.org/configuration/nextjs#middleware
+// this makes all pages require auth by default
+
+export { default } from "next-auth/middleware"
+
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth/next';
+import { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-// import { isEmailInAllowedList } from '@/utils'
+import { isEmailInAllowedList } from '@/utils'
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -11,6 +12,14 @@ export const authOptions = {
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account && profile && account.provider === "google") {
+        return profile.email_verified && isEmailInAllowedList({ email: profile.email! })
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  }
 }
 
 export default NextAuth(authOptions)
