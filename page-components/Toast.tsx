fb27767ddb6818c2bfc11f8b5c0fbf9e4cfc07c7
aktsbot <box.ashishk@gmail.com>
2023-03-16 23:39:33 +0530

    add toast component

---
+++ page-components/Toast.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertColor, AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function Toast({
+  handleClose,
+  open,
+  type,
+  message,
+}: {
+  handleClose: () => void;
+  open: boolean;
+  type: AlertColor;
+  message: string;
+}) {
+  return (
+    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={type} sx={{ width: "100%" }}>
+        {message}{" "}
+      </Alert>
+    </Snackbar>
+  );
+}
