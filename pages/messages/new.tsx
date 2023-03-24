5461de89a11b7cecc3c80b025a4954de941eb29b
aktsbot <box.ashishk@gmail.com>
2023-03-24 23:00:56 +0530

    add new message page

--- pages/messages/new.tsx
+++ pages/messages/new.tsx
@@ -1,3 +1,6 @@
+import { useState, useContext } from "react";
+import { useRouter } from "next/router";
+
 import Typography from "@mui/material/Typography";
 
 import Box from "@mui/material/Box";
@@ -5,25 +8,92 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
 import Layout from "@/page-components/Layout";
+import { AppContext } from "@/contexts/AppContext";
 
 export default function NewMessage() {
+  const router = useRouter();
+  const { addAlert } = useContext(AppContext);
+
+  const [message, setMessage] = useState({
+    content: "",
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const target = e.target;
+    const name = target.name;
+
+    setMessage((prev) => ({
+      ...prev,
+      [name]: target.value,
+    }));
+  }
+
+  const postData = async () => {
+    try {
+      const res = await fetch("/api/messages", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(message),
+      });
+
+      // Throw error with status code in case Fetch API req failed
+      if (!res.ok) {
+        throw new Error(res.status.toString());
+      }
+
+      addAlert({
+        message: "Message ha been added",
+        type: "success",
+      });
+      router.push("/messages");
+    } catch (error) {
+      addAlert({
+        message: "Failed to add message",
+        type: "error",
+      });
+    }
+  };
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(message);
+    postData();
+  }
+
   return (
     <Layout title="Add a message">
       <Typography variant="h4" component="h1" mt={2}>
         Add a message
       </Typography>
 
-      <Box component="form" noValidate autoComplete="off" mt={2}>
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        mt={2}
+        onSubmit={handleSubmit}
+      >
         <TextField
           id="message-content"
           label="Message content"
           multiline
           rows={4}
           fullWidth
+          name="content"
+          onChange={handleChange}
         />
 
         <Box mt={2}>
-          <Button variant="contained">Create message</Button>
+          <Button
+            variant="contained"
+            type="submit"
+            disabled={message.content.length === 0}
+          >
+            Create message
+          </Button>
         </Box>
       </Box>
     </Layout>
