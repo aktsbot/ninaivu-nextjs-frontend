ab154d1b2ae7899306de54bea8adb2e5ae891959
aktsbot <box.ashishk@gmail.com>
2023-03-29 01:08:59 +0530

    wip: Message receipts

--- pages/api/patients/messages.ts
+++ pages/api/patients/messages.ts
@@ -3,6 +3,7 @@ import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
 
 import dbConnect from "@/lib/dbConnect";
+import MessageReceipt from "@/models/MessageReceipt";
 import Patient from "@/models/Patient";
 import Message from "@/models/Message";
 
@@ -22,20 +23,35 @@ export default async function handler(
   await dbConnect();
 
   switch (method) {
-    case "get":
+    case "GET":
+      console.log("getting called");
       try {
         let patientUuid = req.query.patient;
+        console.log(patientUuid);
         if (!patientUuid) {
           return res.status(400).json({ success: false });
         }
 
+        const patientInfo = await Patient.findOne(
+          {
+            uuid: patientUuid,
+          },
+          { _id: 1 }
+        );
+
         // TODO:
         // fix this
+        const receipts = await MessageReceipt.find({
+          patient: patientInfo._id,
+        }).populate("message");
         res.status(200).json({
           success: true,
-          data: {},
+          data: {
+            receipts,
+          },
         });
       } catch (error) {
+        console.log(error);
         res.status(400).json({ success: false });
       }
       break;
