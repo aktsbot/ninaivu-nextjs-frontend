12a6238790aa7275a4a67b1d2f167fbc1441399b
aktsbot <box.ashishk@gmail.com>
2023-04-11 23:51:20 +0530

    wip: message reports api

---
+++ pages/api/message-reports/index.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+
+import dbConnect from "@/lib/dbConnect";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
+  }
+
+  // console.log("session in handler ", session);
+  const loggedInUser = session.user;
+
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      let fromDate = new Date();
+      fromDate.setDate(fromDate.getDate() - 7);
+      let toDate = new Date();
+      try {
+        res.status(200).json({});
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
