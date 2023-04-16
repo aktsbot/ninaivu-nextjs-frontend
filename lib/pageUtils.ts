7c73ea185d6729d2f8aecb0933abd75817a4ca3c
aktsbot <box.ashishk@gmail.com>
2023-04-17 00:01:34 +0530

    hook up stats api

--- lib/pageUtils.ts
+++ lib/pageUtils.ts
@@ -1,5 +1,6 @@
 import { AlertColor } from "@mui/material";
 import { v4 as uuid4 } from "uuid";
+import { parseISO, format } from "date-fns";
 
 export const makeAlertObj = ({
   type,
@@ -31,4 +32,16 @@ export const getFormattedDate = ({ date }: { date: Date }) => {
     strDay = `0${day}`;
   }
   return `${year}-${strMonth}-${strDay}`;
+};
+
+export const getFormattedDateTime = ({
+  dateString,
+}: {
+  dateString: string | null;
+}) => {
+  if (!dateString) {
+    return "";
+  }
+  const date = parseISO(dateString);
+  return format(date, "dd/LL/yyyy hh:mm aaa");
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 
@@ -5,8 +7,43 @@ import ReportTable from "@/page-components/ReportTable";
 import StatCard from "@/page-components/StatCard";
 import Layout from "@/page-components/Layout";
 import { Box } from "@mui/material";
+import { getFormattedDateTime } from "@/lib/pageUtils";
 
 export default function Home() {
+  const [stats, setStats] = useState({
+    patients: 0,
+    messages: 0,
+    ranOn: null,
+    credits: 0,
+  });
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    async function getStats() {
+      try {
+        const res = await fetch("/api/stats?", {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          signal: signal,
+        }).then((r) => r.json());
+        setStats({ ...res.data });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getStats();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
     <Layout title="Home">
       <Typography variant="h4" component="h1" mt={2}>
@@ -15,16 +52,32 @@ export default function Home() {
 
       <Grid container spacing={4} mt={2}>
         <Grid item xs={6}>
-          <StatCard itemHead={"12"} itemText={"patients added"} />
+          <StatCard
+            itemHead={stats.patients.toString()}
+            itemText={"patients added"}
+          />
         </Grid>
         <Grid item xs={6}>
-          <StatCard itemHead={"9"} itemText={"messages added"} />
+          <StatCard
+            itemHead={stats.messages.toString()}
+            itemText={"messages added"}
+          />
         </Grid>
         <Grid item xs={6}>
-          <StatCard itemHead={"07/04/2023 at 04:50 pm"} itemText={"last run"} />
+          <StatCard
+            itemHead={
+              stats.ranOn
+                ? getFormattedDateTime({ dateString: stats.ranOn })
+                : "-"
+            }
+            itemText={"last run"}
+          />
         </Grid>
         <Grid item xs={6}>
-          <StatCard itemHead={"54"} itemText={"credits left"} />
+          <StatCard
+            itemHead={stats.credits.toString()}
+            itemText={"credits left"}
+          />
         </Grid>
       </Grid>
 
